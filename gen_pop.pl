d7e70189cb098f1b28583c37e5ace6e4b87bfae6
David Mertens <dcmertens.perl@gmail.com>
2015-03-07 00:02:35 -0600

    Generator also produced thetas for me

--- gen_pop.pl
+++ gen_pop.pl
@@ -4,5 +4,8 @@ use warnings;
 use PDL;
 my $N_osc = shift || die "You must give a number of oscillators\n";
 zeros($N_osc)->grandom->qsort->wcols("population.txt");
+
 my $pi = atan2(-1, 0);
-(zeros($N_osc)->random * 2 * $pi)->wcols("positions.txt");
+my $thetas = zeros($N_osc)->random * 2 * $pi;
+$thetas -= $pi;
+$thetas->wcols("positions.txt");
